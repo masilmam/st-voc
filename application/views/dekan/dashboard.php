fd30bd0940c4cebaa3e12edd643591254dbaed70
Ilmam Fauzi <ilmam.javajin@gmail.com>
2019-10-01 14:04:36 +0700

    Menghilangkan comment alert tidak terpakai

--- application/views/dekan/dashboard.php
+++ application/views/dekan/dashboard.php
@@ -23,16 +23,6 @@ function tanggal($tgl) {
 ?>
 
       <div class="content mt-3">
-        <!-- alert template -->
-        <!-- <div class="col-sm-12">
-          <div class="alert  alert-success alert-dismissible fade show" role="alert">
-            <span class="badge badge-pill badge-success">Success</span> You successfully read this important alert message.
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-        </div>  -->
-
         <div class="animated fadeIn">
           <div class="row">
             <div class="col-md-12">
